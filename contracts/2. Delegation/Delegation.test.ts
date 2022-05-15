ad752fce78adc2c7d6773f77d237adef9a1c2f79
Abhi Gulati <abhi@indeed.com>
2022-05-14 22:26:52 -0500

    Solve Delegation

--- contracts/2. Delegation/Delegation.test.ts
+++ contracts/2. Delegation/Delegation.test.ts
@@ -1,5 +1,5 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
 
 let victim: any;
 let attacker: any;
@@ -7,23 +7,25 @@ let hacker: any;
 let delegateContract: any;
 let deployer: any;
 
-describe("Attacking Delegation", function () {
-  beforeEach(async () => {
-    [hacker, deployer] = await ethers.getSigners();
-    const DelegateContract = await ethers.getContractFactory("Delegate");
-    delegateContract = await DelegateContract.connect(deployer).deploy(
-      deployer.address
-    );
-    const Victim = await ethers.getContractFactory("Delegation");
-    victim = await Victim.connect(deployer).deploy(delegateContract.address);
-    const Attacker = await ethers.getContractFactory("AttackingDelegation");
-    attacker = await Attacker.deploy(victim.address);
-  });
+describe('Attacking Delegation', function () {
+    beforeEach(async () => {
+        [hacker, deployer] = await ethers.getSigners();
+        const DelegateContract = await ethers.getContractFactory('Delegate');
+        delegateContract = await DelegateContract.connect(deployer).deploy(
+            deployer.address
+        );
+        const Victim = await ethers.getContractFactory('Delegation');
+        victim = await Victim.connect(deployer).deploy(
+            delegateContract.address
+        );
+        const Attacker = await ethers.getContractFactory('AttackingDelegation');
+        attacker = await Attacker.deploy(victim.address);
+    });
 
-  // Get this to pass!
-  it("Succesfully taken ownership", async () => {
-    await attacker.hackContract({ gasLimit: 30000000 });
-    const owner = await victim.owner();
-    expect(owner).to.be.equal(attacker.address);
-  });
+    // Get this to pass!
+    it('Succesfully taken ownership', async () => {
+        await attacker.hackContract({ gasLimit: 30000000 });
+        const owner = await victim.owner();
+        expect(owner).to.be.equal(attacker.address);
+    });
 });
