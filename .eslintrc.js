a333f7a3141b13d5243af828c8fbf52516b65d82
Abhi Gulati <abhi@indeed.com>
2022-05-14 22:13:17 -0500

    Try to fix linting in VSCode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,32 @@
 module.exports = {
-  env: {
-    browser: false,
-    es2021: true,
-    mocha: true,
-    node: true,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
+    env: {
+        browser: false,
+        es2021: true,
+        mocha: true,
+        node: true
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'standard',
+        // 'plugin:prettier/recommended',
+        'plugin:node/recommended'
     ],
-    "node/no-unpublished-import": [
-      "error",
-      {
-        allowModules: ["chai"],
-      },
-    ],
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12
+    },
+    rules: {
+        'node/no-unsupported-features/es-syntax': [
+            'error',
+            { ignores: ['modules'] }
+        ],
+        'node/no-unpublished-import': [
+            'error',
+            {
+                allowModules: ['chai']
+            }
+        ],
+        indent: ['error', 4],
+        semi: [2, 'always']
+    }
 };
