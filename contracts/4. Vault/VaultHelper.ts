e8dbed0a1c0af7a0751c44e439f37507fa8fbca6
Abhi Gulati <abhi@indeed.com>
2022-05-17 22:29:27 -0500

    Solve Vault

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -2,8 +2,9 @@ import { ethers, waffle } from 'hardhat';
 
 const helper = async (victim: any) => {
     const provider = ethers.provider;
-    const balance = await provider.getStorageAt(victim.address, 1);
-    console.log(balance);
+    const password = await provider.getStorageAt(victim.address, 1);
+
+    await victim.unlock(password);
 };
 
 export default helper;
